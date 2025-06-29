49f1488b8e4ba20ead8a594d6170b0a71198d627
n2hq <n2hq.co@gmail.com>
2025-06-29 17:05:34 +0100

    update commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Garssete - Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            .flex_container{
+                display:flex; 
+                justify-content:center; 
+                align-items:center; 
+                height:100vh;
+            }
+
+            .centered_div{
+                width:auto; 
+                height:100px; 
+                text-align:center; 
+                line-height:100px;
+                font-family:'Arial';
+                font-size:17px; 
+            }
+        </style>
+    </head>
+    <body>
+    <div class="flex_container">
+        <div class="centered_div">
+            Hang on for a minute and refresh...
+        </div>
+    </div>
+
+    </body>
+</html>
