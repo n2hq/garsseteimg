f7a506ca4c081cdbc9af39ee6314e17d011a4fef
n2hq <n2hq.co@gmail.com>
2025-07-02 21:04:28 +0100

    Prod: update commit

--- commands_prod.sh
+++ commands_prod.sh
@@ -3,15 +3,34 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "🛠  Building project..."
-#npm run build:test
+
+echo "🔁 Setting remote to PROD repo..."
+#git remote add origin https://github.com/n2hq/garsseteimg_prod.git
+
+echo "🌿  Renaming branch to main..."
+git branch -M main
+
+git remote set-url origin https://github.com/n2hq/garsseteimg_prod.git
+
+
+echo "🧪 Switching to 'main' branch..."
+git checkout main || git checkout -b main
+
+
+echo "🔄 Pulling latest changes..."
+#git pull origin main
+
+
+echo "🛠  Building PROD..."
 npm run build:prod
+
 
 echo "📦  Staging changes..."
 git add .
 
+
 echo "✅  Committing changes..."
-git commit -m "update commit"
+git commit -m "Prod: update commit" || echo "⚠️ No changes to commit."
 
 echo "🌿  Renaming branch to main..."
 git branch -M main
