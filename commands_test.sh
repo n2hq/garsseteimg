dd5155d8eca4ab958485d1913085a9df6ee9fb1a
n2hq <n2hq.co@gmail.com>
2025-07-02 20:53:53 +0100

    Test: update commit

--- commands_test.sh
+++ commands_test.sh
@@ -3,15 +3,29 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "🛠  Building project..."
+
+echo "🔁 Setting remote to TEST repo..."
+git remote set-url origin https://github.com/n2hq/garsseteimg.git
+
+
+echo "🧪 Switching to 'main' branch..."
+git checkout main || git checkout -b main
+
+
+echo "🔄 Pulling latest changes..."
+git pull origin main
+
+
+echo "🛠  Building TEST..."
 npm run build:test
 #npm run build:prod
 
 echo "📦  Staging changes..."
 git add .
 
+
 echo "✅  Committing changes..."
-git commit -m "update commit"
+git commit -m "Test: update commit" || echo "⚠️ No changes to commit."
 
 echo "🌿  Renaming branch to main..."
 git branch -M main
