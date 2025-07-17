ff870464381d29b1f183ffbd63b39cc2a5b32ca4
n2hq <n2hq.co@gmail.com>
2025-07-17 13:19:50 +0100

    Prod: update commit

--- server.mjs
+++ server.mjs
@@ -73,7 +73,7 @@ app.use(morgan("tiny"));
 // handle SSR requests
 app.all("*", remixHandler);
 
-const port = process.env.PORT || PORT;
+const port = process.env.VITE_PORT || PORT;
 app.listen(port, () =>
   console.log(`Express server listening at http://localhost:${port}`)
 );
